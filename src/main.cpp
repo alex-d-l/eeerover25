fe8d6c06541d1475d7162ecf5bb136b4f6c3de85
Alexander Lewis <alexander.lewis2004@outlook.com>
2024-06-17 18:43:00 +0100

    improvements to code

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,22 @@ const int groupNumber = 25;
 
 // Global Variables:
 int currentSpeed = 0;
-float null_point;
+float null_point = 0;
 String receivedName = "";
 String receivedPolarity = "";
 String receivedInfraredFreq = "";
 String receivedRadioFreq = "";
+
+// Variables to store time measurements
+volatile unsigned long lastRisingEdgeTimeRadio = 0;
+volatile unsigned long currentRisingEdgeTimeRadio = 0;
+volatile unsigned long edgeCountRadio = 0;
+float frequencyRadio = 0;
+
+volatile unsigned long lastRisingEdgeTimeInfra = 0;
+volatile unsigned long currentRisingEdgeTimeInfra = 0;
+volatile unsigned long edgeCountInfra = 0;
+float frequencyInfra = 0;
 
 WiFiWebServer server(80);
 
@@ -32,21 +43,7 @@ void readPolarity();
 void readInfrared();
 void readRadio();
 void handleRisingEdge();
-
-// Variables to store time measurements
-volatile unsigned long lastRisingEdgeTimeRadio = 0;
-volatile unsigned long currentRisingEdgeTimeRadio = 0;
-volatile unsigned long edgeCountRadio = 0;
-float frequencyRadio = 0;
-
-unsigned long lastRisingEdgeTimeInfrared = 0;
-unsigned long currentRisingEdgeTimeInfrared = 0;
-unsigned long edgeCountInfrared = 0;
-float frequencyInfrared = 0;
-
-int threshold;  // Initial threshold value
-unsigned long lastThresholdUpdateTime = 0;
-const unsigned long thresholdUpdateInterval = 1000;  // Update threshold every 1000 ms
+void handleRisingEdgeInfra();
 
 
 void setup() {
@@ -62,8 +59,8 @@ void setup() {
   // Analogue Pin for Polarity
   pinMode(A2, INPUT);
 
-  // Analogue Pin for Infrared
-  pinMode(A1, INPUT);
+  // Digital Pin for Infrared
+  pinMode(9, INPUT);
 
   // Digital Pin for Radio
   pinMode(8, INPUT);
@@ -76,13 +73,6 @@ void setup() {
 
   Serial.begin(9600);
 
-  float temp = 0;
-  for(int i = 0; i < 10; i++) {
-    temp = (3.3 * analogRead(A2)) / 1023;
-    null_point += temp;
-    delay(5);
-  }
-  null_point = null_point / 10;
   // Wait 10s for the serial connection before proceeding
   // This ensures you can see messages from startup on the monitor
   // Remove this for faster startup when the USB host isn't attached
@@ -95,10 +85,6 @@ void setup() {
     Serial.println(F("WiFi shield not present"));
     while (true);
   }
-
-  // Configure the static IP address if group number is set
-  // if (groupNumber)
-  //   WiFi.config(IPAddress(192,168,0,groupNumber+1));
 
   // Attempt to connect to WiFi network
   Serial.print(F("Connecting to WPA SSID: "));
@@ -117,7 +103,6 @@ void setup() {
   
   server.begin();
 
-
   Serial.print(F("HTTP server started @ "));
   Serial.println(static_cast<IPAddress>(WiFi.localIP()));
 
@@ -126,6 +111,7 @@ void setup() {
   Serial.println("UART Connection is ready!");
   
   attachInterrupt(digitalPinToInterrupt(8), handleRisingEdge, RISING);
+  attachInterrupt(digitalPinToInterrupt(9), handleRisingEdgeInfra, RISING);
 
 }
 
@@ -209,7 +195,6 @@ void handleCommand() {
 
 }
 
-
 // Generate a 404 response with details of the failed request
 void handleNotFound() {
   String message = F("File Not Found\n\n"); 
@@ -251,7 +236,7 @@ void readName() {
 
   while (Serial1.available() > 0) {
     // for debugging:
-    Serial.println("Serial1 is available! (readName())");
+    Serial.println("Serial1 is available!");
 
     char incomingChar = Serial1.read();
     if (incomingChar == '#') {
@@ -278,10 +263,10 @@ bool isLetter(char c) {
 
 
 void readPolarity() {
-  //float null_point = 1.70; // might need calibration (POLARITY)
+  float null_point = 1.45; // might need calibration (POLARITY)
   float displacement = 0.05; // this value will need calibration as well (POLARITY)
-  
   float mag_read = (3.3 * analogRead(A2)) / 1023;
+  Serial.println(mag_read);
 
   if(mag_read > null_point + displacement) {
     receivedPolarity = "North";
@@ -309,24 +294,26 @@ void handleRisingEdge() {
 
 void readRadio() {
   static unsigned long lastAttemptTimeRadio = 0;
-  const unsigned long timeout = 50; // 50 ms timeout
+  const unsigned long timeout = 50; // 50 ms timeout  
 
   if (millis() - lastAttemptTimeRadio >= timeout) {
     if (edgeCountRadio >= 10) {
       unsigned long period = currentRisingEdgeTimeRadio - lastRisingEdgeTimeRadio;
       frequencyRadio = 1000000.0 / period;
-      if (((frequencyRadio > 95) && (frequencyRadio < 145)) || ((frequencyRadio > 175) && (frequencyRadio < 225))) {
-        receivedRadioFreq = String(frequencyRadio) + " Hz";
+      if((frequencyRadio > 95) && (frequencyRadio < 145)) {
+        receivedRadioFreq = "120 Hz";
+      } 
+      else if((frequencyRadio > 175) && (frequencyRadio < 225)) {
+        receivedRadioFreq = "200 Hz";
       } 
       else {
         // For debugging
-        receivedRadioFreq = String(frequencyRadio) + " Hz"; 
-        // receivedRadioFreq = "Not Detected (Not within range)"; // UNCOMMENT
+        receivedRadioFreq = String(frequencyRadio) + " - Not Within Range"; 
       }
       edgeCountRadio = 0;
     } 
     else {
-      receivedRadioFreq = "Not Detected (Not enough rising edges)";
+      receivedRadioFreq = "Not Detected";
     }
 
     // Reset the timing for the next measurement
@@ -338,115 +325,41 @@ void readRadio() {
 }
 
 
-// TEST WITH DIGITAL PIN
-// void readInfrared() {
-//   unsigned long startTime = 0;
-//   unsigned long currentTime = 0;
+void handleRisingEdgeInfra() {
+  lastRisingEdgeTimeInfra = currentRisingEdgeTimeInfra;
+  currentRisingEdgeTimeInfra = micros();
+  edgeCountInfra++;
+}
 
-//   // Measure max and min - this way we find amplitude and can find threshold
-//   unsigned long start = millis();
-//   int maxRead = 0;
-//   int minRead = 1023;
-//   int currentRead;
-
-//   // Measure for 10 milliseconds
-//   while (millis() - start < 10) {
-//     currentRead = analogRead(A1);
-//     if (currentRead > maxRead) {
-//       maxRead = currentRead;
-//     }
-//     if (currentRead < minRead) {
-//       minRead = currentRead;
-//     }
-//   }
-
-//   int threshold = minRead + (maxRead - minRead) / 2; // Average to find midpoint
-
-//   Serial.print("Threshold: ");
-//   Serial.print(threshold);
-
-//   // Wait for the first rising edge
-//   while (analogRead(A1) < threshold);
-//   startTime = micros();
-
-//   // Initialize variables to store the time intervals
-//   unsigned long intervals[5];
-//   int count = 0;
-
-//   while (count < 5) {
-//     // Wait for the signal to go above the threshold
-//     while (analogRead(A1) < threshold);
-//     // Record the current time when the signal rises
-//     currentTime = micros();
-//     // Store the interval
-//     intervals[count] = currentTime - startTime;
-//     // Update the start time
-//     startTime = currentTime;
-//     count++;
-//   }
-
-//   // Calculate the average period
-//   unsigned long totalPeriod = 0;
-//   for (int i = 0; i < 5; i++) {
-//     totalPeriod += intervals[i];
-//   }
-//   unsigned long averagePeriod = totalPeriod / 5;
-//   float frequency = 1000000.0 / averagePeriod;
-
-//   // Print the frequency
-//   Serial.print(", Frequency: ");
-//   Serial.print(frequency);
-//   Serial.println(" Hz");
-// }
 
 void readInfrared() {
-  unsigned long startTime = 0;
-  unsigned long currentTime = 0;
+  static unsigned long lastAttemptTimeInfra = 0;
+  const unsigned long timeout = 50; // 50 ms timeout
 
-  // Measure max and min  - this way we find amplitude and can find threshold
-  unsigned long start = millis();
-  int maxRead = 0;
-  int minRead = 1023;
-  int currentRead;
+  if (millis() - lastAttemptTimeInfra >= timeout) {
+    if (edgeCountInfra >= 10) {
+      unsigned long period = currentRisingEdgeTimeInfra - lastRisingEdgeTimeInfra;
+      frequencyInfra = 1000000.0 / period;
+      if((frequencyInfra > 328) && (frequencyInfra < 378)) {
+        receivedInfraredFreq = "353 Hz";
+      } 
+      else if((frequencyInfra > 546) && (frequencyInfra < 596)) {
+        receivedInfraredFreq = "571 Hz";
+      } 
+      else {
+        // For debugging
+        receivedInfraredFreq = String(frequencyInfra) + " - Not Within Range"; 
+      }
+      edgeCountInfra = 0;
+    } 
+    else {
+      receivedInfraredFreq = "Not Detected";
+    }
 
-  // Measure for 100 milliseconds
-  while (millis() - start < 50) {
-    currentRead = analogRead(A1);
-    if (currentRead > maxRead) {
-      maxRead = currentRead;
-    }
-    if (currentRead < minRead) {
-      minRead = currentRead;
-    }
+    // Reset the timing for the next measurement
+    lastAttemptTimeInfra = millis();
+
+    // For debugging
+    Serial.println("Received Infrared: " + receivedInfraredFreq);
   }
-
-  int threshold = minRead + (maxRead - minRead) / 2; // Average to find midpoint
-
-  // Wait for the first rising edge
-  while (analogRead(A1) < threshold);
-  startTime = micros();
-
-  // wait for 10 waves
-  //might be bugs due to not leaving the loop
-  if(threshold > 50){//if statement might fix this bug
-    int count = 0;
-    while (count < 5) {
-      while (analogRead(A1) >= threshold); // Wait for the signal to drop below the threshold
-      while (analogRead(A1) < threshold);  // Wait for the signal to go above the threshold
-      count++;
-    }
-  }
-  currentTime = micros();
-
-  // Calculate the average period of one wavelength
-  unsigned long period = (currentTime - startTime) / 5;
-  float frequency = 1000000.0 / period;
-
-  // Print the frequency
-  Serial.print("Infrared: ");
-  Serial.println(frequency);
-
-  receivedInfraredFreq = String(frequency);
-
-  // can print only when the output is in range - for example 350 - 400 hz and 550 to 600 HZ
 }
